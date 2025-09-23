523734cf15fbb549467af8acde43ea0fb0d82104
suman291 <das806490@gmail.com>
2025-09-23 22:12:43 +0530

    api updates

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protect routes with the auth:sanctum middleware
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    // Other protected API routes
+});
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
